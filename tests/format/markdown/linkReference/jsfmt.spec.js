d005cae8f2f1296e05195fc4ef6065f9a71783f4
Javier Jaramago Fernández <jaramago.fernandez.javier@gmail.com>
2024-04-12 19:28:46 +0200

    Update tests with new Markdown option 'breakDefinitions'

--- tests/format/markdown/linkReference/jsfmt.spec.js
+++ tests/format/markdown/linkReference/jsfmt.spec.js
@@ -1,2 +1,6 @@
 runFormatTest(import.meta, ["markdown"], { proseWrap: "always" });
+runFormatTest(import.meta, ["markdown"], {
+  breakDefinitions: false,
+  proseWrap: "always"
+});
 runFormatTest(import.meta, ["markdown"], { proseWrap: "never" });
